eaf51b286c1318430e815e32240097ee12698f0a
Syed Haider Hassan <pakistanihaider@gmail.com>
2016-07-14 16:20:25 +0500

    client controller added for retrival of key

--- application/controllers/Clients.php
+++ application/controllers/Clients.php
@@ -9,4 +9,40 @@ class Clients extends CI_Controller {
 
 		$this->load->view('get_access_token');
 	}
+	function native_curl($new_name, $new_email)
+	{
+		$username = 'admin';
+		$password = '1234';
+
+    // Alternative JSON version
+    // $url = 'http://twitter.com/statuses/update.json';
+    // Set up and execute the curl process
+		$curl_handle = curl_init();
+		curl_setopt($curl_handle, CURLOPT_URL, 'http://localhost/restserver/index.php/example_api/user/id/1/format/json');
+		curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($curl_handle, CURLOPT_POST, 1);
+		curl_setopt($curl_handle, CURLOPT_HTTPHEADER,  $header_data);
+		curl_setopt($curl_handle, CURLOPT_POSTFIELDS, array(
+			'name' => $new_name,
+			'email' => $new_email
+			));
+
+    // Optional, delete this line if your API is open
+		curl_setopt($curl_handle, CURLOPT_USERPWD, $username . ':' . $password);
+
+		$buffer = curl_exec($curl_handle);
+		curl_close($curl_handle);
+
+		$result = json_decode($buffer);
+
+		if(isset($result->status) && $result->status == 'success')
+		{
+			echo 'User has been updated.';
+		}
+
+		else
+		{
+			echo 'Something has gone wrong';
+		}
+	}
 }
